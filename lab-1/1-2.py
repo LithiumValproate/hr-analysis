4d72d5c02e23c8767057c21d63e72ea73fb2e981
LithiumValproate <kisara@folate.li>
2026-03-25 10:17:31 +0800

    Change branch name

--- lab-1/1-2.py
+++
@@ -1,8 +0,0 @@
-def fib(n: int):
-    a, b = 1, 1
-    for _ in range(n):
-        yield a
-        a, b = b, a + b
-
-
-print(*fib(20), sep=', ')
--- lab-1/1-3.py
+++
@@ -1,7 +0,0 @@
-def peach(d, total_days):
-    if d == total_days:
-        return 1
-    return 2 * (peach(d + 1, total_days) + 1)
-
-
-print(peach(1, 10))
